b1cbff2d069ead45ed014763d138511c05d8580c
Lemon Sama <burrisc24@gmail.com>
2022-04-02 20:27:58 -0400

    created page template module, added file writing

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-console.log(profileDataArgs);
-
-
-
+const fs = require('fs');
+profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
+const generatePage = require('./scr/page-template.js');
 
 
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw err;
 
-
-
-
-
-
-
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => console.log(profileItem))
-};
-
-printProfileData(profileDataArgs);
+    console.log('Portfolio complete! Check out index.html to see the output')
+});
